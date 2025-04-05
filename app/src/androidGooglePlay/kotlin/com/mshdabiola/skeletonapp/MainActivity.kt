f3e3d70bee1308b18fe388b806fddb51962be83c
mshdabiola <mshdabiola@gmail.com>
2025-04-05 08:38:30 +0100

    Refactor: Rename package from skeletonapp to wikipedia_common
    This change updates the package name in `MessageService.kt` and `MainActivity.kt` from `com.mshdabiola.skeletonapp` to `com.mshdabiola.wikipedia_common`.

--- app/src/androidGooglePlay/kotlin/com/mshdabiola/skeletonapp/MainActivity.kt
+++ app/src/androidGooglePlay/kotlin/com/mshdabiola/skeletonapp/MainActivity.kt
@@ -2,7 +2,7 @@
  *abiola 2024
  */
 
-package com.mshdabiola.skeletonapp
+package com.mshdabiola.wikipedia_common
 
 import android.graphics.Color
 import android.os.Bundle
@@ -23,8 +23,8 @@ import com.google.firebase.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.remoteconfig.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
-import com.mshdabiola.skeletonapp.ui.SkeletonApp
-import com.mshdabiola.skeletonapp.ui.shouldUseDarkTheme
+import com.mshdabiola.wikipedia_common.ui.SkeletonApp
+import com.mshdabiola.wikipedia_common.ui.shouldUseDarkTheme
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
--- app/src/androidGooglePlay/kotlin/com/mshdabiola/skeletonapp/MessageService.kt
+++ app/src/androidGooglePlay/kotlin/com/mshdabiola/skeletonapp/MessageService.kt
@@ -2,7 +2,7 @@
  *abiola 2024
  */
 
-package com.mshdabiola.skeletonapp
+package com.mshdabiola.wikipedia_common
 
 // import androidx.work.Worker
 // import androidx.work.WorkerParameters
